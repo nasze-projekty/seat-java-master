c491e27be42840ee883a51fe8924fc6fbae28cc0
Dhananjaya CB <dhananjaya.cb@roche.com>
2021-01-23 12:38:54 +0800

    Reoved unused tests

--- src/test/java/restfulbooker/api/dto/booking/BookingResponse.java
+++
@@ -1,8 +0,0 @@
-package restfulbooker.api.dto.booking;
-
-
-import com.framework.api.dto.AbstractDTO;
-
-public class BookingResponse extends AbstractDTO<BookingResponse> {
-    public Booking booking;
-}
--- src/test/java/restfulbooker/api/dto/booking/CreateBookingResponse.java
+++
@@ -1,9 +0,0 @@
-package restfulbooker.api.dto.booking;
-
-
-import com.framework.api.dto.AbstractDTO;
-
-public class CreateBookingResponse extends AbstractDTO<CreateBookingResponse> {
-    public Booking booking;
-    public int bookingid;
-}
